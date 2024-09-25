3162a2e4d15dddc36f246fee0c648dbeda9238dd
John Kowalsky <john.kowalsky@petiq.com>
2024-09-25 10:49:16 -0600

    completed 2.1

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.HandlerFunc(fileserverHandler)))
 	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/metrics", apiCfg.getMetrics)
+	mux.HandleFunc("/reset", apiCfg.resetMetrics)
 
 	srv := &http.Server{
 		Addr:    ":8080",
@@ -18,8 +30,33 @@ func main() {
 	}
 }
 
+func fileserverHandler(w http.ResponseWriter, r *http.Request) {
+	http.StripPrefix("/app/", http.FileServer(http.Dir("./"))).ServeHTTP(w, r)
+}
+
 func healthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("ok"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	metricsCount := strconv.Itoa(int(cfg.fileserverHits.Load()))
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits: " + metricsCount))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Metrics reset"))
 }
